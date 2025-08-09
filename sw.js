5573bb8d14ca25c600b79448172553fcb2d0ca4e
Nancy Zhang <nancy.zhang@enteligent.com>
2025-08-09 19:37:24 +0800

    Initial commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+const VERSION = 'v1.0.0';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(VERSION).then((cache) => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== VERSION).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        if (event.request.url.startsWith(self.location.origin)) {
+          caches.open(VERSION).then((cache) => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => cached)
+    )
+  );
+});
